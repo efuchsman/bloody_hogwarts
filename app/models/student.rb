aed8b76537f8288f77fe65d99e5103b1e6fb9aa3
Eli Fuchsman <efuchsman@gmail.com>
2022-10-19 16:41:34 -0600

    wip US4

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
   validates :house, presence: true
   has_many :student_courses
   has_many :courses, through: :student_courses
+
+  def self.average_age
+    average(:age).round(1)
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe 'instance methods' do
+    it '#average_age' do
+      student_1 = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
+      student_2 = Student.create!(name: "Harry Potter", age: 11, house: "Gryffindoor")
+      student_3 = Student.create!(name: "Rusty Shackleford", age: 13, house: "Gryffindoor")
+
+      expect(Student.average_age).to eq(12.7)
+    end
+  end
 end
